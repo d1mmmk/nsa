421b76cb4e7a62d2bbd75d9d4aee56bc6c6e48b6
d1mmmk <d1mmmk@gmail.com>
2018-07-13 10:18:49 +0000

    fix

--- src/wp-nsa/footer.php
+++ src/wp-nsa/footer.php
@@ -23,6 +23,40 @@
 		    });
 		});
 	})();
+	
+	(function(){
+		
+		var glitchHead = document.querySelector('.glitch-head'),
+			glitch = glitchHead.querySelector('.glitch__img'),
+			glitchEyes = glitchHead.querySelector('.glitch__eyes');
+			
+		var animations = [{
+				target: glitch,
+				timing: { min: 2, max: 6 }
+			},{
+				target: glitchEyes,
+				timing: { min: 1, max: 3 }
+			}];
+
+		function glitchAmin(animation){
+			animation.target.classList.remove('animation');
+			clearTimeout(animation.to);
+			var timeout = Math.floor(Math.random() * animation.timing.max) + animation.timing.min;
+			animation.to = setTimeout(function() {
+				animation.target.classList.add(('animation'));
+			}, timeout * 1000);
+		};
+		animations.forEach(function(animation){
+			glitchAmin(animation);
+		});
+		window.onanimationend = function(event){
+			animations.forEach(function(animation){
+				if (event.target === animation.target) {
+					glitchAmin(animation);
+				}
+			});
+		}
+	})()
 </script>
 
 <?php wp_footer(); ?>
